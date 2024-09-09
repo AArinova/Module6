19080bf5bc34e2aff719903f9a31af7e2c4c949a
AArinova <aliya-arinova@yandex.ru>
2024-09-09 19:25:59 +0700

    Add files via upload

---
+++ module_6_2.py
@@ -0,0 +1,34 @@
+class Vehicle:
+    owner : str # владелец транспорта
+    __model : str #модель (марка) транспорта
+    __engine_power : int # мощность двигателя
+    __color : str
+    __COLOR_VARIANTS = ['magenta', 'black', 'white', 'khaki']
+
+    def get_model(self):
+        return f"Модель: {self.__model}."
+
+    def get_horsepower(self):
+        return f"Мощность двигателя: {self.__engine_power}."
+    def get_color(self):
+        return f"Цвет: {self.__color}."
+
+    def print_info(self):
+        print( self.get_model())
+        print( self.get_horsepower())
+        print(self.get_color);
+        print("Владелец: ", self.owner)
+
+    def set_color(self, new_color):
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
+
+    def new_color(self, new_color):
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
+        else:
+            print(f"Нельзя сменить цвет на {new_color}.")
+
+class Sedan(Vehicle):
+    pass
+
